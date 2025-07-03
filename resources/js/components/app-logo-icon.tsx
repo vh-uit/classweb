8e1d5707debf161c043f6a854f92516ad4a98763
Lê Việt Hoàng <24520546@gm.uit.edu.vn>
2025-07-03 21:13:21 +0700

    Updates application name and adds new logo
    Updates the application name to "ClassWeb" and introduces a new application logo.
    
    The logo is a book/education base with inner pages/content and network/connection nodes to represent collaboration.
    
    Removes the old logo.

--- resources/js/components/app-logo-icon.tsx
+++ resources/js/components/app-logo-icon.tsx
@@ -2,11 +2,48 @@ import { SVGAttributes } from 'react';
 
 export default function AppLogoIcon(props: SVGAttributes<SVGElement>) {
     return (
-        <svg {...props} viewBox="0 0 40 42" xmlns="http://www.w3.org/2000/svg">
+        <svg {...props} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            {/* Book/Education base */}
             <path
                 fillRule="evenodd"
                 clipRule="evenodd"
-                d="M17.2 5.63325L8.6 0.855469L0 5.63325V32.1434L16.2 41.1434L32.4 32.1434V23.699L40 19.4767V9.85547L31.4 5.07769L22.8 9.85547V18.2999L17.2 21.411V5.63325ZM38 18.2999L32.4 21.411V15.2545L38 12.1434V18.2999ZM36.9409 10.4439L31.4 13.5221L25.8591 10.4439L31.4 7.36561L36.9409 10.4439ZM24.8 18.2999V12.1434L30.4 15.2545V21.411L24.8 18.2999ZM23.8 20.0323L29.3409 23.1105L16.2 30.411L10.6591 27.3328L23.8 20.0323ZM7.6 27.9212L15.2 32.1434V38.2999L2 30.9666V7.92116L7.6 11.0323V27.9212ZM8.6 9.29991L3.05913 6.22165L8.6 3.14339L14.1409 6.22165L8.6 9.29991ZM30.4 24.8101L17.2 32.1434V38.2999L30.4 30.9666V24.8101ZM9.6 11.0323L15.2 7.92117V22.5221L9.6 25.6333V11.0323Z"
+                d="M4 2C3.44772 2 3 2.44772 3 3V19C3 20.1046 3.89543 21 5 21H19C19.5523 21 20 20.5523 20 20V4C20 2.89543 19.1046 2 18 2H4ZM5 4H18V19H5V4Z"
+                opacity="0.9"
+            />
+            
+            {/* Inner pages/content */}
+            <path
+                d="M6 6H16V8H6V6Z"
+                opacity="0.7"
+            />
+            <path
+                d="M6 10H14V12H6V10Z"
+                opacity="0.7"
+            />
+            <path
+                d="M6 14H12V16H6V14Z"
+                opacity="0.7"
+            />
+            
+            {/* Network/connection nodes representing collaboration */}
+            <circle cx="16" cy="8" r="1.5" />
+            <circle cx="14" cy="12" r="1.5" />
+            <circle cx="12" cy="16" r="1.5" />
+            
+            {/* Connection lines */}
+            <path
+                d="M15.2 9.2L14.8 10.8"
+                stroke="currentColor"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                opacity="0.8"
+            />
+            <path
+                d="M13.2 13.2L12.8 14.8"
+                stroke="currentColor"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                opacity="0.8"
             />
         </svg>
     );
